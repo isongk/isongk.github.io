82cd8a5930485cbd0dffea21aab41bee9ff86f46
isongk <34570117+isongk@users.noreply.github.com>
2017-12-15 17:04:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Discount</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="HandheldFriendly" content="True">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	
+	<link rel="apple-touch-icon" href="images/main.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="images/main76.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="images/main120.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="images/main152.png">
+	
+	<style>
+@import url(https://fonts.googleapis.com/css?family=Roboto:300);
+.page {
+  width: 360px;
+  padding: 8% 0 0;
+  margin: auto;
+}
+
+@media (max-width:500px){
+.page { width: 90%;}
+
+
+}
+.form {
+  position: relative;
+  z-index: 1;
+  background: #FFFFFF;
+  max-width: 360px;
+  margin: 0 auto 100px;
+  padding: 45px;
+  text-align: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}
+.form input {
+  font-family: "Roboto", sans-serif;
+  outline: 0;
+  background: #f2f2f2;
+  width: 100%;
+  border: 0;
+  margin: 0 0 15px;
+  padding: 15px;
+  box-sizing: border-box;
+  font-size: 14px;	
+}
+#button {
+  font-family: "Roboto", sans-serif;
+  text-transform: uppercase;
+  outline: 0;
+  background: #4CAF50;
+ 
+  border: 0;
+  padding: 15px;
+  color: #FFFFFF;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+#button:hover,#button:active,#button:focus {
+  background: #43A047;
+}
+.form .message {
+  margin: 15px 0 0;
+  color: #b3b3b3;
+  font-size: 12px;
+}
+.form .message a {
+  color: #4CAF50;
+  text-decoration: none;
+}
+.form .register-form {
+  display: none;
+}
+.container {
+  position: relative;
+  z-index: 1;
+  max-width: 300px;
+  margin: 0 auto;
+}
+.container:before, .container:after {
+  content: "";
+  display: block;
+  clear: both;
+}
+.container .info {
+  margin: 50px auto;
+  text-align: center;
+}
+.container .info h1 {
+  margin: 0 0 15px;
+  padding: 0;
+  font-size: 36px;
+  font-weight: 300;
+  color: #1a1a1a;
+}
+.container .info span {
+  color: #4d4d4d;
+  font-size: 12px;
+}
+.container .info span a {
+  color: #000000;
+  text-decoration: none;
+}
+.container .info span .fa {
+  color: #EF3B3A;
+}
+body {
+  background: #76b852; /* fallback for old browsers */
+  background: -webkit-linear-gradient(right, #76b852, #8DC26F);
+  background: -moz-linear-gradient(right, #76b852, #8DC26F);
+  background: -o-linear-gradient(right, #76b852, #8DC26F);
+  background: linear-gradient(to left, #76b852, #8DC26F);
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;      
+}
+
+	
+.discount{
+	text-align: center;
+	font-size: 20px;
+	padding:10px;
+	border: 1px dashed rgba(190, 196, 202,1);
+}
+.discount span{
+	display:block;
+}
+
+#percent{
+	font-size: 50px;
+	color:#DA2C1C;
+}
+#save{
+	color:#025949;
+}
+</style>
+</head>
+
+<body>
+
+
+<div class="page">
+  <div class="form">
+   <div class="discount">
+		<span id="percent"> 50% OFF</span>
+		<!--div class="discount">
+			<span id="form">Total 500 $</span>
+			<span id="save">Discount 250 $</span>
+			<span id="to">Net 250 $</span>
+		 </div-->
+	</br>
+	<table style="width:100%;text-align:right;">
+		<TR>
+			<TD >Total</TD>
+			<TD id="form">500</span></TD>
+			<TD>$</TD>
+		</TR>
+		<TR>
+			<TD>Discount</TD>
+			<TD id="save">250</TD>
+			<TD>$</TD>
+		</TR>
+		<TR>
+			<TD class="title">Net</TD>
+			<TD id="to">250</TD>
+			<TD>$</TD>
+		</TR>
+	</table>
+	</div>
+	<div class="discount" style="display:none;margin-top:10px;">
+			<span id="vat">+VAT 50% = 750 $</span>
+	</div>
+	   
+   </br><!--hr style="border-color:rgba(190, 196, 202, 0.3);"></br-->
+   
+    <form class="login-form">
+      <input type="text" placeholder="value $" id="value"/>
+      <input type="text" placeholder="discount %" id="discount"/>
+      <div id="button" onclick="getdiscount()">calculator</div>
+      <p class="message">This Cal For discount <a href="../discount/">Refresh</a></p>
+    </form>
+  </div>
+</div>
+
+<script type="text/javascript">
+function getdiscount(){
+	//alert('click');
+	var v = document.getElementById('value').value;
+	var d = document.getElementById('discount').value;
+	
+	var s = (v*d)/100;
+	var t = v-s;
+	document.getElementById('percent').innerHTML=d+"% OFF";
+	document.getElementById('form').innerHTML=v;
+	document.getElementById('save').innerHTML=s;
+	document.getElementById('to').innerHTML=t;
+	
+	var vt = parseInt(s)+parseInt(v);
+	document.getElementById('vat').innerHTML="+VAT "+d+"% = "+vt+" $";
+	
+	
+}
+</script>
+	
+
+</body>
+</html>
